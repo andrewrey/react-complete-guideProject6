3d1629166c2af537c22c0c040d154834259c9697
Andrew Reynolds <andrew_reynolds@mac.com>
2021-07-06 13:51:48 -0700

    created reducer function for useReducer in cart context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,12 +1,37 @@
+import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = ({ children }) => {
-  const addItem = () => {};
+const defaultState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItem = (id) => {};
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const updatedItems = [...state.items, action.item];
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+      return { items: updatedItems, totalAmount: updatedTotalAmount };
+    case "REMOVE":
+      break;
+    default:
+      return defaultState;
+  }
+};
+
+const CartProvider = ({ children }) => {
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultState);
+  const addItem = (item) => {
+    dispatchCartAction({ type: "ADD", item });
+  };
+
+  const removeItem = (id) => {
+    dispatchCartAction({ type: "REMOVE", id });
+  };
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem,
     removeItem,
   };
